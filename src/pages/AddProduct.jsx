3f714d8bb9664bfac4da17b380df0dd2c711e326
Matthieu Chatelin <matthieu888@gmail.com>
2025-02-08 15:36:56 +0100

    Add input verification on Login, Register & AddProduct

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
 import styles from "../styles/AddProduct.module.css";
+import { useState, useRef } from "react";
 
 export default function AddProduct() {
   const [name, setName] = useState("");
@@ -8,22 +8,69 @@ export default function AddProduct() {
   const [category, setCategory] = useState([]);
   const [stock, setStock] = useState(0);
   const [image, setImage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const nameRef = useRef();
+  const descriptionRef = useRef();
+  const priceRef = useRef();
+  const categoryRef = useRef();
+  const stockRef = useRef();
+  const imageRef = useRef();
 
   const handleAddProduct = async () => {
-    try {
-      const res = await fetch("http://localhost:3000/products/", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        credentials: "include",
-        body: JSON.stringify({ name, description, price, category: [category], stock, imageUrl: image }),
-      });
-      if (res.ok) {
-        const data = await res.json();
-        console.log(data);
+    if (name.trim() === "") {
+      setErrorMessage("Please enter the product name");
+      nameRef.current.focus();
+      return;
+    } else if (description.trim() === "") {
+      setErrorMessage("Please enter the product description");
+      descriptionRef.current.focus();
+      return;
+    } else if (price === 0) {
+      setErrorMessage("Please enter the product price");
+      priceRef.current.focus();
+      return;
+    } else if (category.length === 0) {
+      setErrorMessage("Please enter at least one category");
+      categoryRef.current.focus();
+      return;
+    } else if (stock === 0) {
+      setErrorMessage("Please enter the product stock");
+      stockRef.current.focus();
+      return;
+    } else if (image.trim() === "") {
+      setErrorMessage("Please enter a product url");
+      imageRef.current.focus();
+      return;
+    } else {
+      setErrorMessage("");
+
+      try {
+        const res = await fetch("http://localhost:3000/products/", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          credentials: "include",
+          body: JSON.stringify({ name, description, price, category, stock, imageUrl: image }),
+        });
+        if (res.ok) {
+          const data = await res.json();
+          console.log(data);
+        }
+      } catch (error) {
+        console.error("Error saving new product:", error);
       }
-    } catch (error) {
-      console.error("Error saving new product:", error);
     }
+  };
+
+  const handleCategoryChange = (e) => {
+    const options = e.target.options;
+    const selectedCategories = [];
+    for (let i = 0; i < options.length; i++) {
+      if (options[i].selected) {
+        selectedCategories.push(options[i].value);
+      }
+    }
+    setCategory(selectedCategories);
   };
 
   return (
@@ -31,9 +78,10 @@ export default function AddProduct() {
       <h2>AddProduct Page</h2>
       <form action="submit" onSubmit={async (e) => e.preventDefault()} className={styles.form}>
         <label htmlFor="name">Product Name</label>
-        <input type="text" id="name" placeholder="Product Name" value={name} onChange={(e) => setName(e.target.value)} />
+        <input ref={nameRef} type="text" id="name" placeholder="Product Name" value={name} onChange={(e) => setName(e.target.value)} />
         <label htmlFor="description">Description</label>
         <input
+          ref={descriptionRef}
           type="text"
           id="description"
           placeholder="Description"
@@ -41,9 +89,9 @@ export default function AddProduct() {
           onChange={(e) => setDescription(e.target.value)}
         />
         <label htmlFor="price">Price</label>
-        <input type="number" id="price" placeholder="Price in €" value={price} onChange={(e) => setPrice(e.target.value)} />
+        <input ref={priceRef} type="number" id="price" placeholder="Price in €" value={price} onChange={(e) => setPrice(e.target.value)} />
         <label htmlFor="category">Category</label>
-        <select name="category" id="category" value={category} onChange={(e) => setCategory(e.target.value)}>
+        <select ref={categoryRef} name="category" id="category" value={category} onChange={handleCategoryChange} multiple>
           <option value="audio">Audio</option>
           <option value="retro-gaming">Retro Gaming</option>
           <option value="films-and-tv-series">Films & TV Séries</option>
@@ -53,12 +101,15 @@ export default function AddProduct() {
           <option value="food-and-sweets">Food & Sweets</option>
         </select>
         <label htmlFor="stock">Stock</label>
-        <input type="number" id="stock" placeholder="Stock" value={stock} onChange={(e) => setStock(e.target.value)} />
+        <input ref={stockRef} type="number" id="stock" placeholder="Stock" value={stock} onChange={(e) => setStock(e.target.value)} />
         <label htmlFor="image">Image URL</label>
-        <input type="text" id="image" placeholder="Image URL" value={image} onChange={(e) => setImage(e.target.value)} />
+        <input ref={imageRef} type="text" id="image" placeholder="Image URL" value={image} onChange={(e) => setImage(e.target.value)} />
         <button type="submit" onClick={handleAddProduct}>
           Add Product
         </button>
+        <p className={styles.errorText} role="alert">
+          {errorMessage ? errorMessage : <span style={{ visibility: "hidden" }}>Invisible</span>}
+        </p>
       </form>
     </div>
   );
--- src/pages/Connection.jsx
+++ src/pages/Connection.jsx
@@ -1,5 +1,5 @@
 import styles from "../styles/Connection.module.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { login } from "../store/userReducer";
 
@@ -7,47 +7,106 @@ export default function Connection() {
   const [firstname, setFirstname] = useState("");
   const [lastname, setLastname] = useState("");
   const [email, setEmail] = useState("");
+  const [emailLog, setEmailLog] = useState("");
   const [password, setPassword] = useState("");
+  const [passwordLog, setPasswordLog] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [errorMessageLog, setErrorMessageLog] = useState("");
 
   const dispatch = useDispatch();
 
-  const handleRegisterSubmit = async () => {
-    try {
-      const res = await fetch("http://localhost:3000/users/register", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        credentials: "include",
-        body: JSON.stringify({ firstname, lastname, email, password }),
-      });
+  const firstnameRef = useRef(null);
+  const lastnameRef = useRef(null);
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const emailLogRef = useRef(null);
+  const passwordLogRef = useRef(null);
 
-      if (res.ok) {
-        const data = await res.json();
-        dispatch(
-          login({ firstname: data.user.firstname, lastname: data.user.lastname, publicId: data.user.publicId, role: data.user.role })
-        );
+  const handleRegisterSubmit = async () => {
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+    if (firstname.trim() === "") {
+      setErrorMessage("Please enter your firstname");
+      firstnameRef.current.focus();
+      return;
+    } else if (lastname.trim() === "") {
+      setErrorMessage("Please enter your lastname");
+      lastnameRef.current.focus();
+      return;
+    } else if (email.trim() === "") {
+      setErrorMessage("Please enter your email");
+      emailRef.current.focus();
+      return;
+    } else if (!regex.test(email)) {
+      setErrorMessage("Invalid email address");
+      emailRef.current.focus();
+      return;
+    } else if (password.trim() === "") {
+      setErrorMessage("Please enter a password");
+      passwordRef.current.focus();
+      return;
+    } else {
+      setErrorMessage("");
+
+      try {
+        const res = await fetch("http://localhost:3000/users/register", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          credentials: "include",
+          body: JSON.stringify({ firstname, lastname, email, password }),
+        });
+
+        if (res.ok) {
+          const data = await res.json();
+          dispatch(
+            login({ firstname: data.user.firstname, lastname: data.user.lastname, publicId: data.user.publicId, role: data.user.role })
+          );
+        } else {
+          setErrorMessage("This user already exists");
+        }
+      } catch (error) {
+        console.error("Error during registration:", error);
+        setErrorMessage("Unable to connect to the server. Please try again later.");
       }
-    } catch (error) {
-      console.error("Error during registration:", error);
     }
   };
 
   const handleLoginSubmit = async () => {
-    try {
-      const res = await fetch("http://localhost:3000/users/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        credentials: "include",
-        body: JSON.stringify({ email, password }),
-      });
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
 
-      if (res.ok) {
-        const data = await res.json();
-        dispatch(
-          login({ firstname: data.user.firstname, lastname: data.user.lastname, publicId: data.user.publicId, role: data.user.role })
-        );
+    if (emailLog.trim() === "") {
+      setErrorMessageLog("Please enter your email");
+      emailLogRef.current.focus();
+      return;
+    } else if (!regex.test(emailLog)) {
+      setErrorMessageLog("Invalid email address");
+      emailLogRef.current.focus();
+      return;
+    } else if (passwordLog.trim() === "") {
+      setErrorMessageLog("Please enter your password");
+      passwordLogRef.current.focus();
+      return;
+    } else {
+      setErrorMessageLog("");
+
+      try {
+        const res = await fetch("http://localhost:3000/users/login", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          credentials: "include",
+          body: JSON.stringify({ emailLog, passwordLog }),
+        });
+
+        if (res.ok) {
+          const data = await res.json();
+          dispatch(
+            login({ firstname: data.user.firstname, lastname: data.user.lastname, publicId: data.user.publicId, role: data.user.role })
+          );
+        }
+      } catch (error) {
+        console.error("Error during login:", error);
+        setErrorMessageLog("Unable to connect to the server. Please try again later.");
       }
-    } catch (error) {
-      console.error("Error during login:", error);
     }
   };
 
@@ -59,6 +118,7 @@ export default function Connection() {
           <form action="submit" onSubmit={async (e) => e.preventDefault()} className={styles.registerForm}>
             <label htmlFor="firstname">Firstname</label>
             <input
+              ref={firstnameRef}
               type="text"
               id="firstname"
               placeholder="Enter your firstname"
@@ -67,6 +127,7 @@ export default function Connection() {
             />
             <label htmlFor="lastname">Lastname</label>
             <input
+              ref={lastnameRef}
               type="text"
               id="lastname"
               placeholder="Enter your lastname"
@@ -75,6 +136,7 @@ export default function Connection() {
             />
             <label htmlFor="emailRegister">Email</label>
             <input
+              ref={emailRef}
               type="email"
               id="emailRegister"
               placeholder="Enter your email"
@@ -83,6 +145,7 @@ export default function Connection() {
             />
             <label htmlFor="passwordRegister">Password</label>
             <input
+              ref={passwordRef}
               type="password"
               id="passwordRegister"
               placeholder="Enter your password"
@@ -92,23 +155,37 @@ export default function Connection() {
             <button type="submit" onClick={handleRegisterSubmit}>
               Register
             </button>
+            <p className={styles.errorText} role="alert">
+              {errorMessage ? errorMessage : <span style={{ visibility: "hidden" }}>Invisible</span>}
+            </p>
           </form>
         </div>
         <div className={styles.loginSection}>
           <form action="submit" onSubmit={async (e) => e.preventDefault()} className={styles.loginForm}>
             <label htmlFor="emailLogin">Email</label>
-            <input type="email" id="emailLogin" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input
+              ref={emailLogRef}
+              type="email"
+              id="emailLogin"
+              placeholder="Enter your email"
+              value={emailLog}
+              onChange={(e) => setEmailLog(e.target.value)}
+            />
             <label htmlFor="passwordLogin">Password</label>
             <input
+              ref={passwordLogRef}
               type="password"
               id="passwordLogin"
               placeholder="Enter your password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              value={passwordLog}
+              onChange={(e) => setPasswordLog(e.target.value)}
             />
             <button type="submit" onClick={handleLoginSubmit}>
               Login
             </button>
+            <p className={styles.errorText} role="alert">
+              {errorMessageLog ? errorMessageLog : <span style={{ visibility: "hidden" }}>Invisible</span>}
+            </p>
           </form>
         </div>
       </div>
