809f1662f3b4d9ec097a5f63afd3618f6846d1d4
Matthieu Chatelin <matthieu888@gmail.com>
2025-02-08 20:32:54 +0100

    Add persistor for users infos

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import App from "./App";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import store from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persistor } from "./store/store";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userReducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedUserReducer = persistReducer(persistConfig, userReducer);
 
 const store = configureStore({
   reducer: {
-    user: userReducer,
+    user: persistedUserReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
+export const persistor = persistStore(store);
 export default store;
