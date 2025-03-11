2701a6c43a447d924e6a31bee37d58ece7a9a06d
Matthieu Chatelin <matthieu888@gmail.com>
2025-03-11 11:01:15 +0100

    Add PaymentCancel page

--- src/pages/PaymentCancel.jsx
+++ src/pages/PaymentCancel.jsx
@@ -1,8 +1,20 @@
+import styles from "../styles/paymentCancel.module.css";
+import { useNavigate } from "react-router-dom";
+import { CreditCardTwoTone } from "@mui/icons-material";
+
 export default function PaymentCancel() {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h2>Payment Canceled</h2>
-      <p>Your payment was canceled. Please try again.</p>
+    <div className={styles.main}>
+      <div className={styles.container}>
+        <p className={styles.title}>Payment Canceled</p>
+        <p>Your payment was canceled. Please try again.</p>
+        <button className={`btn ${styles.validateBtn}`} onClick={() => navigate("/order")}>
+          <CreditCardTwoTone style={{ fontSize: 22 }} />
+          Try again
+        </button>
+      </div>
     </div>
   );
 }
